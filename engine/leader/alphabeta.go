75e9ddb6489c397588f38a21a4721fe5735e1ff1
Kristian Darlington <kristian.f.darlington@gmail.com>
2020-07-06 01:52:21 -0700

    added early cancellation functionality via context

--- engine/leader/alphabeta.go
+++ engine/leader/alphabeta.go
@@ -11,11 +11,15 @@ func (l *Leader) alphabeta(ctx context.Context, b *pb.Board, depth int, alpha fl
 		return l.evaluate(ctx, b)
 	}
 	boardChan := make(chan *pb.Board)
+	// ensures that all child processes of this alpha-beta call will exit if their
+	// branch is pruned or the function is otherwise done
+	ctx2, cancel := context.WithCancel(ctx)
+	defer cancel()
 	if maximizingPlayer {
 		value := math.Inf(-1) // negative infinity
-		go l.expand(ctx, b, boardChan)
+		go l.expand(ctx2, b, boardChan)
 		for board := range boardChan {
-			value = math.Max(value, float64(l.alphabeta(ctx, board, depth - 1, alpha, beta, false)))
+			value = math.Max(value, float64(l.alphabeta(ctx2, board, depth - 1, alpha, beta, false)))
 			alpha = math.Max(alpha, value)
 			if beta <= alpha {
 				break
@@ -24,9 +28,9 @@ func (l *Leader) alphabeta(ctx context.Context, b *pb.Board, depth int, alpha fl
 		return float32(value)
 	} else {
 		value := math.Inf(1) // positive infinity
-		go l.expand(ctx, b, boardChan)
+		go l.expand(ctx2, b, boardChan)
 		for board := range boardChan {
-			value = math.Min(value, float64(l.alphabeta(ctx, board, depth - 1, alpha, beta, true)))
+			value = math.Min(value, float64(l.alphabeta(ctx2, board, depth - 1, alpha, beta, true)))
 			beta = math.Min(beta, value)
 			if beta <= alpha {
 				break
