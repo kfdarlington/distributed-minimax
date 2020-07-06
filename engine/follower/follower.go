c85255364e92fe9a2d2c9a285a246b1413fea320
Kristian Darlington <kristian.f.darlington@gmail.com>
2020-07-06 14:36:26 -0700

    edited follower temporarily for testing

--- engine/follower/follower.go
+++ engine/follower/follower.go
@@ -2,33 +2,43 @@ package follower
 
 import (
 	"context"
+	"github.com/google/logger"
 	"github.com/kristian-d/distributed-minimax/engine/pb"
+	"io/ioutil"
+	"math/rand"
 )
 
 type minimaxServer struct {
+	logger *logger.Logger
 	pb.UnimplementedMinimaxServer
 }
 
-func (server *minimaxServer) GetExpansion(req *pb.ExpandRequest, stream pb.Minimax_GetExpansionServer) error {
-	reply := &pb.ExpandReply{
-		Board: req.Board,
-	}
-	if err := stream.Send(reply); err != nil {
-		return err
+func (s *minimaxServer) GetExpansion(req *pb.ExpandRequest, stream pb.Minimax_GetExpansionServer) error {
+	for i := 0; i < 2; i++ { // TODO fix
+		reply := &pb.ExpandReply{
+			Board: req.Board,
+		}
+		s.logger.Infof("send %d", i)
+		if err := stream.Send(reply); err != nil {
+			s.logger.Errorf("error when sending on stream err=%v", err)
+			return err
+		}
 	}
 	return nil
 }
 
-func (server *minimaxServer) GetEvaluation(ctx context.Context, req *pb.EvaluateRequest) (*pb.EvaluateReply, error) {
+func (s *minimaxServer) GetEvaluation(ctx context.Context, req *pb.EvaluateRequest) (*pb.EvaluateReply, error) {
 	return &pb.EvaluateReply{
-		Score: 500,
+		Score: rand.Float32(),
 	}, nil
 }
 
-func (server *minimaxServer) RequestCancellation(ctx context.Context, req *pb.CancelRequest) (*pb.CancelAck, error) {
+func (s *minimaxServer) RequestCancellation(ctx context.Context, req *pb.CancelRequest) (*pb.CancelAck, error) {
 	return nil, nil
 }
 
 func NewFollower() *minimaxServer {
-	return &minimaxServer{}
+	return &minimaxServer{
+		logger: logger.Init("Follower", true, false, ioutil.Discard),
+	}
 }
