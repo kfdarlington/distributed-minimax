2c591cbad54da1f415744648c09eca69cd61af55
Kristian Darlington <kristian.f.darlington@gmail.com>
2020-08-13 13:17:33 -0700

    added getoriginatingboard function in battlesnake

--- battlesnake/game/game.go
+++ battlesnake/game/game.go
@@ -1,6 +1,10 @@
 package game
 
-import "github.com/kristian-d/distributed-minimax/engine/pb"
+import (
+	"errors"
+	"fmt"
+	"github.com/kristian-d/distributed-minimax/engine/pb"
+)
 
 var Games map[string]*Game
 
@@ -17,6 +21,7 @@ const (
 	LEFT  Move = "left"
 	RIGHT Move = "right"
 	NONE  Move = ""
+	DEFAULT_MOVE Move = "up"
 )
 
 type MoveCoordinate struct {
@@ -61,6 +66,54 @@ func (b *Board) Protobuf() *pb.Board {
 			Values: b.Grid.GetValues(),
 		},
 		Snakes: snakes,
+	}
+}
+
+func GetOriginatingMove(fromBoard *pb.Board, toBoard *pb.Board) (Move, error) {
+	var fromHeadCoord *pb.Board_Snake_Coordinate
+	if fromSnakes, ok := fromBoard.GetSnakes()[ME]; !ok {
+		return NONE, errors.New("we do not exist in snake mapping from originating board")
+	} else if len(fromSnakes.GetBody()) == 0 {
+		return NONE, errors.New("we do not have a head (body length is 0) in snake body from originating board")
+	} else {
+		fromHeadCoord = fromSnakes.GetBody()[0]
+	}
+
+	var toHeadCoord *pb.Board_Snake_Coordinate
+	if toSnakes, ok := toBoard.GetSnakes()[ME]; !ok {
+		return NONE, errors.New("we do not exist in snake mapping from destination board")
+	} else if len(toSnakes.GetBody()) == 0 {
+		return NONE, errors.New("we do not have a head (body length is 0) in snake body from destination board")
+	} else {
+		toHeadCoord = toSnakes.GetBody()[0]
+	}
+
+	xDiff := int64(toHeadCoord.X) - int64(fromHeadCoord.X)
+	yDiff := int64(toHeadCoord.Y) - int64(fromHeadCoord.Y)
+	switch xDiff {
+	case 0:
+		switch yDiff {
+		case 1:
+			return DOWN, nil
+		case -1:
+			return UP, nil
+		default:
+			return NONE, errors.New(fmt.Sprintf("toBoard and fromBoard are not one move apart xDifference=%d yDifference=%d", xDiff, yDiff))
+		}
+	case 1:
+		if yDiff == 0 {
+			return RIGHT, nil
+		} else {
+			return NONE, errors.New(fmt.Sprintf("toBoard and fromBoard are not one move apart xDifference=%d yDifference=%d", xDiff, yDiff))
+		}
+	case -1:
+		if yDiff == 0 {
+			return LEFT, nil
+		} else {
+			return NONE, errors.New(fmt.Sprintf("toBoard and fromBoard are not one move apart xDifference=%d yDifference=%d", xDiff, yDiff))
+		}
+	default:
+		return NONE, errors.New(fmt.Sprintf("toBoard and fromBoard are not one move apart xDifference=%d yDifference=%d", xDiff, yDiff))
 	}
 }
 
